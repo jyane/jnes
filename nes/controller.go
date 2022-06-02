6372848adf8481028278f6f974858216711bac35
Shohei Kamimori <jyane.kstm@gmail.com>
2022-06-02 20:29:35 +0900

    fix controller bugs and implement strobe

--- nes/controller.go
+++ nes/controller.go
@@ -7,6 +7,9 @@ package nes
 
 type button int
 
+// Controller bit assignments, 1 means pressed otherwise 0.
+// bit    7 6      5     4  3    2    1     0
+// button A B Select Start Up Down Left Right
 const (
 	ButtonA button = iota
 	ButtonB
@@ -21,6 +24,7 @@ const (
 type Controller struct {
 	buttons [8]bool
 	index   byte
+	strobe  byte
 }
 
 func NewController() *Controller {
@@ -32,16 +36,24 @@ func (c *Controller) Set(buttons [8]bool) {
 }
 
 func (c *Controller) read() byte {
+	ret := byte(0)
+	if c.index < 8 && c.buttons[c.index] {
+		ret = 1
+	}
 	c.index++
-	if c.index == 8 {
+	if c.strobe&1 == 1 {
 		c.index = 0
-		return 0
 	}
-	if c.buttons[c.index] {
-		return 1
-	}
-	return 0
+	return ret
 }
 
+// write writes strobe.
+// https://bugzmanov.github.io/nes_ebook/chapter_7.html
+// - strobe bit on - controller reports only status of the button A on every read
+// - strobe bit off - controller cycles through all buttons
 func (c *Controller) write(data byte) {
+	c.strobe = data
+	if c.strobe&1 == 1 {
+		c.index = 0
+	}
 }
